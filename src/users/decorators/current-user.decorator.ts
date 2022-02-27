df399f165bb3ea14000f356a98418c862b603c49
Ifycode <marychord@gmail.com>
2022-02-27 01:31:35 +0100

    Update: communicating from interceptor to decorator

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -4,6 +4,7 @@ export const Currentuser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     console.log('Currently signed in user id: ', request.session.userId);
-    return 'Hi there!';
+    console.log('Retrieve current user: ', request.currentUser);
+    return request.currentUser;
   },
 );
