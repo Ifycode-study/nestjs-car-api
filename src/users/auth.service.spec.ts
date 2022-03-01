b3cf447f1f539957613f5bdd4f71074a43dd9f85
Ifycode <marychord@gmail.com>
2022-03-01 03:13:50 +0100

    Update: Improving the file layout

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,28 +3,34 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
 
-  // Create a module (& a Dependncy Injection container)
-  const module = await Test.createTestingModule({
-    providers: [
-      // Providers here are list of classes we want to register in our testing DI container
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // Create a fake copy of the users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  // Reach into the module/DI container to get the AuthService
-  const service = module.get(AuthService);
+    // Create a module (& a Dependncy Injection container)
+    const module = await Test.createTestingModule({
+      providers: [
+        // Providers here are list of classes we want to register in our testing DI container
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    // Reach into the module/DI container to get the AuthService
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
